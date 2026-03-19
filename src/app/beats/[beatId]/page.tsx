65c91ee7880d64df17a7ec2de48f696c45b6c356
SonidoLiquidoGitHu <zakeuno@gmail.com>
2026-03-19 01:30:02 -0600

    Fix @/lib/db import in src/app/beats/[beatId]/page.tsx for Render deployment

--- src/app/beats/[beatId]/page.tsx
+++ src/app/beats/[beatId]/page.tsx
@@ -1,4 +1,4 @@
-import { getClient, initializeDatabase } from "@/lib/db";
+import { getClient, initializeDatabase } from "../../../lib/db";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import {
